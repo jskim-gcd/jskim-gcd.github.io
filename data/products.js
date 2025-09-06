af561d30642c7443efe871c0e44752dac71e9d31
Kim Jinsoo <jskimcs1@gmail.com>
2025-09-06 11:02:29 +0900

    가격 추가

--- data/products.js
+++ data/products.js
@@ -2,61 +2,73 @@ let products = [
     {
         id: 0,
         title: 'Demian',
-        author: 'Hermann Hesse'
+        author: 'Hermann Hesse',
+        price: 7200
     },
     {
         id: 1,
         title: 'The Little Prince',
-        author: 'Antoine de Saint-Exupéry'
+        author: 'Antoine de Saint-Exupéry',
+        price: 2700
     },
     {
         id: 2,
         title: 'The Old Man and the Sea',
-        author: 'Ernest Hemingway'
+        author: 'Ernest Hemingway',
+        price: 7200
     },
     {
         id: 3,
         title: '1984',
-        author: 'George Orwell'
+        author: 'George Orwell',
+        price: 9900
     },
     {
         id: 4,
         title: 'The Vegetarian',
-        author: 'Han Kang'
+        author: 'Han Kang',
+        price: 15300
     },
     {
         id: 5,
         title: 'Sapiens: A Brief History of Humankind',
-        author: 'Yuval Noah Harari'
+        author: 'Yuval Noah Harari',
+        price: 27000
     },
     {
         id: 6,
         title: 'Guns, Germs, and Steel',
-        author: 'Jared Diamond'
+        author: 'Jared Diamond',
+        price: 26820
     },
     {
         id: 7,
         title: 'Cosmos',
-        author: 'Carl Sagan'
+        author: 'Carl Sagan',
+        price: 19800
     },
     {
         id: 8,
-        title: 'Rich Dad Poor Dad',
-        author: 'Robert T. Kiyosaki'
+        title: 'Human Acts',
+        author: 'Han Kang',
+        price: 13500
     },
     {
         id: 9,
         title: 'Atomic Habits',
-        author: 'James Clear'
+        author: 'James Clear',
+        price: 14400
     },
     {
         id: 10,
         title: 'The One Thing',
-        author: 'Gary Keller'
+        author: 'Gary Keller',
+        price: 15120
     },
     {
         id: 11,
         title: 'The Alchemist',
-        author: 'Paulo Coelho'
+        author: 'Paulo Coelho',
+        price: 12150
     }
 ];
