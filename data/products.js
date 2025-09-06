d6b2cced2265337fa71d89bdee841056a9671e4d
Kim Jinsoo <jskimcs1@gmail.com>
2025-09-06 11:24:18 +0900

    데이터 수정

--- data/products.js
+++ data/products.js
@@ -1,41 +1,5 @@
 let products = [
     {
-        id: 0,
-        title: 'Demian',
-        author: 'Hermann Hesse',
-        price: 7200
-    },
-    {
-        id: 1,
-        title: 'The Little Prince',
-        author: 'Antoine de Saint-Exupéry',
-        price: 2700
-    },
-    {
-        id: 2,
-        title: 'The Old Man and the Sea',
-        author: 'Ernest Hemingway',
-        price: 7200
-    },
-    {
-        id: 3,
-        title: '1984',
-        author: 'George Orwell',
-        price: 9900
-    },
-    {
-        id: 4,
-        title: 'The Vegetarian',
-        author: 'Han Kang',
-        price: 15300
-    },
-    {
-        id: 5,
-        title: 'Sapiens: A Brief History of Humankind',
-        author: 'Yuval Noah Harari',
-        price: 27000
-    },
-    {
         id: 6,
         title: 'Guns, Germs, and Steel',
         author: 'Jared Diamond',
