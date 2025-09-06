d58f70d432a874067f8cb66231c1bae0c677fc74
Kim Jinsoo <jskimcs1@gmail.com>
2025-09-06 10:50:44 +0900

    제품 데이터

---
+++ data/products.js
@@ -0,0 +1,62 @@
+let products = [
+    {
+        id: 0,
+        title: 'Demian',
+        author: 'Hermann Hesse'
+    },
+    {
+        id: 1,
+        title: 'The Little Prince',
+        author: 'Antoine de Saint-Exupéry'
+    },
+    {
+        id: 2,
+        title: 'The Old Man and the Sea',
+        author: 'Ernest Hemingway'
+    },
+    {
+        id: 3,
+        title: '1984',
+        author: 'George Orwell'
+    },
+    {
+        id: 4,
+        title: 'The Vegetarian',
+        author: 'Han Kang'
+    },
+    {
+        id: 5,
+        title: 'Sapiens: A Brief History of Humankind',
+        author: 'Yuval Noah Harari'
+    },
+    {
+        id: 6,
+        title: 'Guns, Germs, and Steel',
+        author: 'Jared Diamond'
+    },
+    {
+        id: 7,
+        title: 'Cosmos',
+        author: 'Carl Sagan'
+    },
+    {
+        id: 8,
+        title: 'Rich Dad Poor Dad',
+        author: 'Robert T. Kiyosaki'
+    },
+    {
+        id: 9,
+        title: 'Atomic Habits',
+        author: 'James Clear'
+    },
+    {
+        id: 10,
+        title: 'The One Thing',
+        author: 'Gary Keller'
+    },
+    {
+        id: 11,
+        title: 'The Alchemist',
+        author: 'Paulo Coelho'
+    }
+];
